571afc2a17895826d5c08e3e9a5767a7f25762d8
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-03-15 11:19:33 +0100

    fixed home.jsx

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -14,16 +14,18 @@ export default function Home(){
 
     return (
         <div className="container">
-            <div className="row">
+            <div className="row mb-3">
                 <h1>Or go through our categories</h1>
             </div>
-            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 d-flex gap-3 flex-wrap">
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-5">
             {data.map((item, i) => {
                 return (
-                    <div className="col p-5 bg-dark rounded-3" key={i}>
-                        <div className="p-5 text-center">
-                            <h1 className="fw-bold text-light">{item.strCategory}</h1>
-                            <img className="w-100" src={item.strCategoryThumb} alt="" />     
+                    <div className="col" key={i}>
+                        <div className="col bg-dark rounded-3">
+                            <div className="p-5 text-center">
+                                <h1 className="fw-bold text-light">{item.strCategory}</h1>
+                                <img className="w-100" src={item.strCategoryThumb} alt="" />     
+                            </div>
                         </div>
                     </div>
                 )
