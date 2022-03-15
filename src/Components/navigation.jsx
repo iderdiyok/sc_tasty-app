cd0975d64e4dd2e8e0588148f02f81608dcbd896
SuperCoder <supercoder@SuperCoders-Air.fritz.box>
2022-03-15 16:51:16 +0100

    dont

--- src/Components/navigation.jsx
+++ src/Components/navigation.jsx
@@ -3,17 +3,17 @@ export default function Navigation(){
     return(
         <header>
             <nav>
-                <div class="px-4 py-5 my-5 text-center">
+                <div className="px-4 py-5 my-5 text-center">
                     <div className="col-5 col-md-2 mx-auto">
-                        <img class="mb-4 w-100" src={logo} alt="Tasty-Logo"/>
+                        <img className="mb-4 w-100" src={logo} alt="Tasty-Logo"/>
                     </div>
                     
-                    <h1 class="display-5 fw-bold">Find a recipe, an idea, an inspiration...</h1>
-                    <div class="col-6 mx-auto">
-                        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                            <form class="d-md-flex">
-                                <input class="form-control me-2 col-md-8" type="search" placeholder="Type something to search" aria-label="Search"/>
-                                <button class="btn col-md-4 btn-dark" type="submit">Search</button>
+                    <h1 className="display-5 fw-bold">Find a recipe, an idea, an inspiration...</h1>
+                    <div className="col-6 mx-auto">
+                        <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                            <form className="d-md-flex">
+                                <input className="form-control me-2 col-md-8" type="search" placeholder="Type something to search" aria-label="Search"/>
+                                <button className="btn col-md-4 btn-dark" type="submit">Search</button>
                             </form>
                         </div>
                     </div>
